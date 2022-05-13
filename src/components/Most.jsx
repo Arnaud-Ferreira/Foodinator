9e19b45e43ddbcfc3d4b24a634d017dbd67495bd
Arnaud-Ferreira <ferreira.arnaud@hotmail.fr>
2022-05-13 11:40:17 +0200

    asking api / saving & using the data's from this

--- src/components/Most.jsx
+++ src/components/Most.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import { useEffect, useState } from "react";
 
 function Most() {
+
+    // To save the data's and use it
+    const [most, setMost] = useState([]);
+
+    // we run the getPopular function as soon as the components get mounted
+    useEffect(() => {
+        getPopular();
+    },[]);
+
+    const getPopular = async () => {
+        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=6`);
+        // going to give me a json format to be able to play around with the data
+        const data = await api.json();
+        setMost(data.recipes);
+    }
+
+
     return (
-        <div>Most viewed</div>
+        <div>
+            {most.map((recipe) => {
+                return(
+                    <div key={recipe.id}>
+                       <p>{recipe.title}</p> 
+                    </div>
+                )
+            })}
+        </div>
     )
 }
 
