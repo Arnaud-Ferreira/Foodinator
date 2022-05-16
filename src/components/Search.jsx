821c063a29ad52a99aa0ac43f370bbdbe5c81516
Arnaud-Ferreira <ferreira.arnaud@hotmail.fr>
2022-05-16 18:11:52 +0200

    fixing bugs

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,14 +1,15 @@
-import styled from 'styled-components';
+// import styled from 'styled-components';
 import { useState } from 'react';
 import {FaSearch} from 'react-icons/fa';
-import {useNavigate} from 'react-router-dom';
+import {useNavigate} from "react-router-dom";
+import './Search.scss';
 
 function Search() {
 
   const [input, setInput] = useState("");
 
   // To navigate between pages with the input
-  const navigate = useNavigate();
+  let navigate = useNavigate();
 
   // On enter i want to load up another page
   const submitHandler = (e) => {
@@ -18,48 +19,46 @@ function Search() {
   };
 
   return (
-    <FormStyle onSubmit={submitHandler}>
-        <div>
+        <form className='formstyle' onSubmit={submitHandler}>
           <FaSearch onClick={submitHandler} />
           <input
            onChange={(e) => setInput(e.target.value)}
            type="text"
            value={input} 
           />
-        </div>
-    </FormStyle>
+        </form>
   );
 }
 
-const FormStyle = styled.div`
-    margin: 2rem;
-    position: relative;
-    display: flex;
-    justify-content: center;
-    div {
-        width: 60%;
-        position: relative;
-    }
-    input {
-        border: none;
-        background: linear-gradient(35deg, #494949, #313131);
-        font-size: 1.4rem;
-        color: white;
-        padding: 1rem 3rem;
-        border: none;
-        border-radius: 1rem;
-        outline: none;
-        width: 100%;
-    }
+// const FormStyle = styled.div`
+//     margin: 2rem;
+//     position: relative;
+//     display: flex;
+//     justify-content: center;
+//     div {
+//         width: 60%;
+//         position: relative;
+//     }
+//     input {
+//         border: none;
+//         background: linear-gradient(35deg, #494949, #313131);
+//         font-size: 1.4rem;
+//         color: white;
+//         padding: 1rem 3rem;
+//         border: none;
+//         border-radius: 1rem;
+//         outline: none;
+//         width: 100%;
+//     }
 
-    svg {
-        position: absolute;
-        top: 50%;
-        left: 0%;
-        transform: translate(100%, -50%);
-        color: white;
-        cursor: pointer;
-    }
-`;
+//     svg {
+//         position: absolute;
+//         top: 50%;
+//         left: 0%;
+//         transform: translate(100%, -50%);
+//         color: white;
+//         cursor: pointer;
+//     }
+// `;
 
 export default Search;
