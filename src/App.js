ca925ea9270b0aae2a2dc37981ce2dba76eeda8b
Arnaud-Ferreira <ferreira.arnaud@hotmail.fr>
2022-05-17 15:03:11 +0200

    logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import React from 'react';
 import Pages from './pages/Pages';
 import Category from './components/Category';
 import { BrowserRouter } from 'react-router-dom';
 import Search from './components/Search';
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import Logo from './brand.png';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Nav>
+      <Brand to={"/"}>
+        <img className="logo" src={Logo} alt="logo"></img>
+      </Brand>
+      </Nav>
         <Search />
         <Category />
         <Pages />
@@ -15,4 +25,22 @@ function App() {
   );
 }
 
+const Brand = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: 'Poppins', cursive;
+`;
+
+const Nav = styled.div`
+  padding: 1rem 0rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  svg{
+    font-size: 2rem;
+  }
+`;
+
 export default App;
+
---
+++ src/components/Brand.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Logo from '../brand.png';
+
+function Brand() {
+  return (
+    <img src={Logo} alt="logo" />
+  )
+}
+
+export default Brand;
